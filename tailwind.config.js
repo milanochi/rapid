677f33864533c9a05da14aafc734979e7501ee0a
Ochi Daniel <dtonye338@gmail.com>
2024-05-09 07:37:57 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '760px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        lighBlack: 'rgb(13 13 13)',
+        brightRed: 'hsl(12, 88%, 59%)',
+        darkGreen: 'rgb(67 166 105)',
+        bronzeLight: '#CA8F34',
+        lightGray: '#ADABAB'
+
+      },
+    },
+  },
+  plugins: [],
+}
